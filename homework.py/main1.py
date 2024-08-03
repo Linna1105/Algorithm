3756f30d19df30e1f4cc5892000f24dab58b5458
unknown <Linna.Rin@pnc.lan>
2024-08-03 12:29:18 +0700

    Algorithm

---
+++ homework.py/main1.py
@@ -0,0 +1,30 @@
+# q1
+x = 0
+y = 0
+z = 0
+if x == 5:
+    if y < 6:
+        if z <= 11:
+            print("nothing")
+        else:
+            print(2)
+    else:
+        print(1)
+else:
+    if y > 3:
+        print(3)
+    else:
+        print("nothing")
+# q2
+x = 0
+y = 0
+if x < 4:
+    if y < 2:
+        print(1)
+    else:
+        print("nothing")
+else:
+    if y > 3:
+        print("nothing")
+    else:
+        print(3)
---
+++ homework.py/main5.py
@@ -0,0 +1,5 @@
+userMessage = int(input())
+if userMessage <0:
+    print("Negative Number!")
+else:
+    print("Positive Number!")
